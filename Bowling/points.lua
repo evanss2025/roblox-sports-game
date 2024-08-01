95acb6fa09ad6ca8e3e02affbd5a71e60b616f94
Sophia E <sophialuvv2@gmail.com>
2024-08-01 16:59:43 -0400

    Update points.lua

--- Bowling/points.lua
+++ Bowling/points.lua
@@ -1,11 +1,16 @@
 pointPart = script.Parent
 pointValue = script.Parent.points
-pointsGUItext = script.Parent.pointsGUI.TextLabel
 Pins = script.Parent.Parent.Pins
 pinsCopy = game.ServerStorage.Pins
 scoreDisplay = script.Parent.Parent.scoreDisplay.SurfaceGui.TextLabel
+overallScore = script.Parent.Parent.playerScore.Value
+Round = script.Parent.Parent.Round.Value
+roundLabel = script.Parent.Parent.scoreDisplay2.SurfaceGui.roundLabel
 
-pointsGUItext.Text = pointValue.Value
+scoreDisplay.Text = pointValue.Value
+Round = 1
+roundLabel.Text = "Round: " .. Round
+
 
 pointPart.Touched:Connect(function(p)
 	if p.Name == "bowlingPin" then
@@ -20,9 +25,16 @@ pointPart.Touched:Connect(function(p)
 		
 		else
 			print("There are no pins left")
+			Pins:Destroy()
 			local pinsCopy = pinsCopy:Clone()
-			pinsCopy.Parent = pointPart.Parent.Pins
+			pinsCopy.Parent = pointPart.Parent
+			
+			overallScore = overallScore + pointValue.Value
+			script.Parent.Parent.scoreDisplay2.SurfaceGui.overallScore.Text = overallScore
+
 			pointValue.Value = 0
+			Round = Round + 1
+			roundLabel.Text = "Round: " .. Round
 			scoreDisplay.Text = pointValue.Value
 
 		end
