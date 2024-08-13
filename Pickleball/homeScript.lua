637ebbe02eff14139ffb2dc39a617b0827c0fbd1
Sophia E <sophialuvv2@gmail.com>
2024-08-13 15:09:29 -0400

    Update homeScript.lua
    checking if other gui's are open already, closing if they are when a new one is selected

--- Pickleball/homeScript.lua
+++ Pickleball/homeScript.lua
@@ -8,13 +8,25 @@ script.Parent.Parent.inventoryGUI.Enabled = false
 script.Parent.Parent.settingsGUI.Enabled = false
 
 storeButton.MouseButton1Click:Connect(function()
+	checkEnabled(script.Parent.Parent.inventoryGUI)
+	checkEnabled(script.Parent.Parent.settingsGUI)
 	script.Parent.Parent.storeGUI.Enabled = true
 end)
 
 inventoryButton.MouseButton1Click:Connect(function()
+	checkEnabled(script.Parent.Parent.storeGUI)
+	checkEnabled(script.Parent.Parent.settingsGUI)
 	script.Parent.Parent.inventoryGUI.Enabled = true
 end)
 
 settingsButton.MouseButton1Click:Connect(function()
+	checkEnabled(script.Parent.Parent.inventoryGUI)
+	checkEnabled(script.Parent.Parent.storeGUI)
 	script.Parent.Parent.settingsGUI.Enabled = true
 end)
+
+function checkEnabled(gui)
+	if gui.Enabled == true then
+		gui.Enabled = false
+	end
+end
