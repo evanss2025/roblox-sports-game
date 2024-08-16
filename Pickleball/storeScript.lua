77c55d7994fffd12bd3d2e83d04146b8d36e2304
Sophia E <sophialuvv2@gmail.com>
2024-08-16 18:12:32 -0400

    Update storeScript.lua

--- Pickleball/storeScript.lua
+++ Pickleball/storeScript.lua
@@ -5,16 +5,18 @@ local coinBtn = script.Parent.storeBackground.options:WaitForChild("coinButton")
 
 moneyLabel.Text = "$" .. money.Value
 
+script.Parent.storeBackground.ShopFrame.Visible = true
+
 money:GetPropertyChangedSignal("Value"):Connect(function()
 	moneyLabel.Text = "$" .. money.Value
 end)
 
 paddleBtn.MouseButton1Click:Connect(function()
-	script.Parent.storeBackground.items.TextLabel.Text = "Pickleball Paddle Shop"
+	script.Parent.storeBackground.ShopFrame.Visible = true
 end)
 
 coinBtn.MouseButton1Click:Connect(function()
-	script.Parent.storeBackground.items.TextLabel.Text = "Coin Shop"
+	script.Parent.storeBackground.ShopFrame.Visible = false
 end)
 
 closeButton = script.Parent.storeBackground.closeButton
